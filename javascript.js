2242ad89eb54bf30e421aee0b642e0ad217b5d97
dymjin <dymjin@gmail.com>
2023-07-13 12:59:27 +0200

    Add function for comparing player vs computer choice

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+// function to start each game
+
+//get computer's choice
 let getComputerChoice = () => {
     let randomNumGen = (min, max) => {
         min = Math.ceil(min);
@@ -6,12 +9,57 @@ let getComputerChoice = () => {
     }
     let choice = 0;
     if (randomNumGen(1, 4) === 1) {
-        choice = "Rock";
+        choice = "rock";
     } else if (randomNumGen(1, 4) === 2) {
-        choice = "Paper"
+        choice = "paper"
     } else {
-        choice = "Scissor"
+        choice = "scissors"
     }
     return choice;
 }
-console.log(getComputerChoice());
+// compare playerSelection with computerSelection and see who wins
+let playRound = (playerSelection, computerSelection) => {
+    if (playerSelection.toLowerCase() === "rock") {
+        if (computerSelection === "rock") {
+            console.log("Tie, rock can't beat rock.");
+            return winner = "none";
+        } else if (computerSelection === "paper") {
+            console.log("You lose, paper beats rock.");
+            return winner = "computer";
+        } else {
+            console.log("You win, rock beats scissors.");
+            return winner = "player";
+        }
+    } else if (playerSelection.toLowerCase() === "paper") {
+        if (computerSelection === "rock") {
+            console.log("You win, paper beats rock.");
+            return winner = "player";
+        } else if (computerSelection === "paper") {
+            console.log("Tie, paper can't beat paper.");
+            return winner = "none";
+        } else {
+            console.log("You lose, scissors beat paper.");
+            return winner = "computer";
+        }
+    } else if (playerSelection.toLowerCase() === "scissors") {
+        if (computerSelection === "paper") {
+            console.log("You win, scissors beat paper.");
+            return winner = "player";
+        } else if (computerSelection === "rock") {
+            console.log("You lose, rock beats scissors.");
+            return winner = "computer";
+        } else {
+            console.log("Tie, scissors can't beat scissors.");
+            return winner = "none";
+        }
+    }
+}
+let computerSelection = getComputerChoice();
+let playerSelection = "rock"
+console.log(playerSelection);
+console.log(computerSelection);
+
+playRound(playerSelection, computerSelection);
+
+
+
